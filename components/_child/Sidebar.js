7d3c835d3bc7e5c622a2c763b6286509eddc8830
Megan Matanda <97180810+Megan74@users.noreply.github.com>
2022-09-19 15:43:10 +0200

    Update Sidebar.js

--- components/_child/Sidebar.js
+++ components/_child/Sidebar.js
@@ -11,18 +11,18 @@ export default function Sidebar(){
             <p className="pb-2 ">Hi, I am Megan!<br></br>
              A 30 years old bubbly fashion enthusiast.<br></br>
              Ever since I am a kid, conformity has always been a concept,
-             that I have struggled to grasp my head around. 
+             that I struggle to grasp my head around. 
              Excentric, with a big personality and a very big imagination,
-             my goals have always been centered around the rules that I have specifically designed for my wellbeing, 
-             despite the ones that society and its very perspicuous codes, had thrown at me.
+             my goals have always been centered around the rules that I have specifically designed for my well-being, 
+             despite the very perspicuous ones, that society had thrown at me.
              <br></br>Hence, it has been very natural for me to be drawn into art and creativity.
             
-            In that universe, there is NO rules, NO restrictive access, NO judgements, JUST pure Freedom. 
-            Art possesses that anarchistic power to spread colours and flavours, in a black and white, capitalistic, cold world, where social strats and etiquettes prevail.
+            In that universe, there is NO rules, NO restrictive access, NO judgements, JUST pure Freedom.<br></br> 
+            Art possesses that anarchistic power that spreads colours and flavours, in a black and white, capitalistic, cold world, where social strats and etiquettes prevail.
 
-            <br></br>The beauty in art, is that it gets to touch the humans core soul, no matter the social background, sexual orientation, political/religious beliefs.
+            <br></br>The beauty in art, is that it gets to touch the human's core soul, no matter the social background, sexual orientation or political/religious beliefs.
 
-            <br></br>A powerful, timeless, unifiying, healing power.<br></br>
+            <br></br>A very powerful, timeless, unifiying and healing power ❤️.<br></br>
             </p>
         </div>
         </aside>
